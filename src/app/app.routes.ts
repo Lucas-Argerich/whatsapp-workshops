c46479ac283ab248b2301ca8b417dd7a07187074
Lucas Argerich <lucasargerichcoder@gmail.com>
2026-03-13 14:04:56 -0300

    update angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { ComposerComponent } from './components/composer/composer.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
-export const routes: Routes = [
+export const routes: Route[] = [
   { path: '', redirectTo: 'composer', pathMatch: 'full' },
   { path: 'contacts', component: ContactsComponent },
   { path: 'composer', component: ComposerComponent },
